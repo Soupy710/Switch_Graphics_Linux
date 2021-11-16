c8def171c0033938aa8fa7081a9d73147b9c95a5
Swaroop <swaroopbhat710@gmail.com>
2021-11-16 19:36:56 +0530

    stable v0.1

--- switch_graphics.sh
+++ switch_graphics.sh
@@ -1,5 +1,25 @@
 sudo mv /etc/X11/xorg.conf /etc/X11/xorg.temp
 sudo mv /etc/X11/xorg.bak /etc/X11/xorg.conf
 sudo mv /etc/X11/xorg.temp /etc/X11/xorg.bak
-echo "Swaitching Graphics" 
+echo "Switching Graphics" 
+check_current=$(cat /etc/X11/xorg.conf | grep "Card1")
+echo "$check_current"
+if [[ "$check_current" == "" ]]
+    then 
+        echo "Switching to Intel Integrated GPU... Please wait"
+        sleep 1
+        sudo systemctl stop gdm
+        sleep 2
+        sudo modprobe -r nvidia_drm
+        sudo modprobe -r nvidia
+        sudo systemctl start gdm
+else
+    	echo "Switching to NVIDIA GPU... Please wait"
+    	sleep 1
+        sudo systemctl stop gdm
+        sleep 2
+        sudo modprobe nvidia
+        sudo modprobe nvidia_drm
+        sudo systemctl start gdm
+fi
 #sudo systemctl restart gdm
