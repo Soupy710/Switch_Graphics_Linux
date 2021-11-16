bf9d4e98b45f6b5a3e33174729e77d65fb83f6c3
Swaroop <swaroopbhat710@gmail.com>
2021-11-16 19:19:00 +0530

    v0.1

---
+++ switch_graphics.sh
@@ -0,0 +1,5 @@
+sudo mv /etc/X11/xorg.conf /etc/X11/xorg.temp
+sudo mv /etc/X11/xorg.bak /etc/X11/xorg.conf
+sudo mv /etc/X11/xorg.temp /etc/X11/xorg.bak
+echo "Swaitching Graphics" 
+#sudo systemctl restart gdm
